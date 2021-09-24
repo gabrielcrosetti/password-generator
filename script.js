9ce02a3d8a14c6e32883302d9889b664c4164e0c
Gabriel Crosetti <crosetti.gabriel@gmail.com>
2021-09-23 20:14:44 -0700

    Began changes to last else if statements

--- script.js
+++ script.js
@@ -13,16 +13,25 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specCharacters = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+specCharacters = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+space = []
 
 var numberConfirm
 var specialCharConf
 var lowerCaseConf
 var upperCaseConf
 var lengthConf
+
+var choices;
+
+var toUpper = function (x) {
+return x.toUpperCase()
+}
+
+alphabet2 = alphabet.map(toUpper)
+
 
 function generate() {
   lengthConf = prompt( "Please type out the desired length of your password. Must between 8-128 characters.")
@@ -36,7 +45,7 @@ lengthConf = prompt( "Please type out the desired length of your password. Must 
   alert("The password length must be between 8-128 characters long.")
 lengthConf = prompt( "Please type out the desired length of your password. Must between 8-128 characters.")
 
-}else
+}else {
   alert("Your desired password will have " + lengthConf + " characters.")
   numberConfirm =confirm("Click OK if you would like numbers in your password.")
 
@@ -49,16 +58,33 @@ specialCharConf =confirm ("Click OK if you would like special characters in your
 
 
 
-if (numberConfirm === false && lowerCaseConf === false && upperCaseConf === false && specialCharConf === false){
+}if (numberConfirm === false && lowerCaseConf === false && upperCaseConf === false && specialCharConf === false){
   alert("You must confirm at least one of the options.")
-numberConfirm =confirm("Click OK if you would like numbers in your password.")
 
-lowerCaseConf =confirm("Click OK if you would like lower case letters in your password.")
-
-upperCaseConf =confirm("Click OK if you would like upper case letters in your password.")
-
-specialCharConf =confirm ("Click OK if you would like special characters in your password.")
 }
+
+else if (specialCharConf && numberConfirm && upperCaseConf && lowerCaseConf) {
+
+  choices = specCharacters.concat(numbers, alphabet, alphabet2);
+}
+
+else if (specialCharConf && numberConfirm && upperCaseConf) {
+  choices = specCharacters.concat(numbers, alphabet2);
+
+}
+else if (specialCharConf && numberConfirm && lowerCaseConf) {
+  choices = specCharacters.concat(numbers, alphabet)
+}
+
+else if (confirmCharacter && lowerCaseConf && upperCaseConf) {
+  choices = specCharacters.concat(alphabet, alphabet2);
+}
+
+else if (numberConfirm && lowerCaseConf && upperCaseConf) {
+    choices = specCharacters.concat(alphabet, alphabet2);
+}
+
+
 
 
 
